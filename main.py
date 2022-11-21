24c31e4dd940518d89fdd25d17c2736b690cf4e2
Nettking <m@nettking.no>
2022-11-21 13:57:37 +0100

    Added example states and seperated robot control in file structure

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import defs.control2Robots;
+import defs.Robot_Control.control2Robots;
 import defs.conveyorR2;
 import defs.conveyorSensors;
 import defs.detect2Objects;
 import defs.pickAndPlaceR1;
 import defs.Gripper;
-
+import defs.Gripper;
 
 
 
