66744066eb3d06438e18fce2a6b8a369efb13c3f
Nettking <m@nettking.no>
2022-11-22 15:08:35 +0100

    Example usage of position

---
+++ Position.py
@@ -0,0 +1,51 @@
+class Position:
+    # Initialize class
+    def __init__(self, x, y, z, rx, ry, rz):
+        self.x = x
+        self.y = y    
+        self.z = z
+        self.rx = rx
+        self.ry = ry    
+        self.rz = rz
+    
+    # To string representation
+    def __str__(self):
+        return f"{self.x} {self.y} {self.z} {self.rx} {self.ry} {self.rz}"
+    
+    # Getters
+    def getX(self):
+        return self.x
+
+    def getY(self):
+        return self.y
+    
+    def getZ(self):
+        return self.z
+    
+    def getRX(self):
+        return self.rx
+    
+    def getRY(self):
+        return self.ry
+    
+    def getRZ(self):
+        return self.rz
+
+    # Setters
+    def setX(self, x):
+        self.x = x
+    
+    def setY(self, y):
+        self.y = y
+    
+    def setZ(self, z):
+        self.z = z
+    
+    def setRX(self, rx):
+        self.rx = rx
+    
+    def setX(self, ry):
+        self.ry = ry
+    
+    def setX(self, rz):
+        self.rz = rz
