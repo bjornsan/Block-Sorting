e577db7721e5d4249f9b38fd6afe09a58ad3c41f
Nettking <m@nettking.no>
2022-11-23 15:14:39 +0100

    Added test of usage of states

--- defs/states.py
+++ defs/states.py
@@ -1,9 +1,35 @@
 def isPreSorting():
-    return;
+    return
 
 def isPostSorting():
-    return;
+    return
 
-def isTransporting(type_of_block):
-    type = type_of_block;
-    return type;
+def isTransporting():
+    return
+
+def isMoving():
+    return
+
+
+def determineAvailibleRobots():
+    if isTransporting() == True: 
+        print("Conveyor belt busy - During transport")
+        if isPreSorting() == True: 
+            print("Pre sorting - During transport")
+        if isPostSorting() == True: 
+            print("Post sorting - During transport")
+        if isMoving() == True: 
+            print("Moving - During transport")
+        else:
+            print("Not sorting - During transport")
+
+    else:
+        print("Conveyor belt idle - Ready for transport")
+        if isPreSorting() == True: 
+            print("Pre sorting - idle conveyor belt")
+        if isPostSorting() == True: 
+            print("Post sorting - idle conveyor belt")
+        if isMoving() == True: 
+            print("Moving - idle conveyor belt")
+        else:
+            print("Not sorting - idle conveyor belt")   
--- main.py
+++ main.py
@@ -4,9 +4,12 @@ import defs.Robot_Control.conveyorSensors
 import defs.Robot_Control.detect2Objects
 import defs.Robot_Control.Gripper
 import defs.Robot_Control.pickAndPlaceR1
-import defs.states
+import defs.states as st 
 import defs.Image_Analysis.findCircle
 import defs.Image_Analysis.findSquare
 import defs.Classes.Position
+import time
 
 test = Position(0, 0, 0, 0, 3.14, 0)
+
+
