a4f8a673c5ca8c125b79c0460d931752c4878086
Nettking <m@nettking.no>
2022-11-25 17:44:44 +0100

    update test.py

--- test.py
+++ test.py
@@ -7,19 +7,21 @@ def startConveyorWhenBlockPlaced():
     sensorRight = cS.checkConveyorSensor(cS.sensorRight)
     sensorMiddleLeft = cS.checkConveyorSensor(cS.sensorMiddleLeft)
     sensorMiddleRight = cS.checkConveyorSensor(cS.sensorMiddleRight)
-    if sensorLeft < 50:
+    if sensorLeft > 50:
         # No object in front of sensor left side 
         if sensorRight > 50:
             # No object in front of sensor right side - Ok to travel left to right
             if sensorMiddleRight > 50:
                 # No object in front of sensor middle right
                 if sensorMiddleLeft > 50:
-                    # Initial start position
-                    conveyor.setConveyorSpeed(0.05)
-                    conveyor.startConveyor()
-                    conveyor.time.sleep(1)
-                    conveyor.stopConveyor()
-                    conveyor.rob2.close()
+    elif sensorLeft < 50:
+        # Initial start position
+        conveyor.setConveyorSpeed(0.05)
+        conveyor.startConveyor()
+        conveyor.time.sleep(1)
+        conveyor.stopConveyor()
+        conveyor.rob2.close()
+         
 
 while True:
     startConveyorWhenBlockPlaced()
