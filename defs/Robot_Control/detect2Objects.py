d8fbb55054d519883ab252736cd5422ac9fbcc95
Nettking <m@nettking.no>
2022-11-25 17:06:47 +0100

    Added functionality for both cameras

--- defs/Robot_Control/detect2Objects.py
+++ defs/Robot_Control/detect2Objects.py
@@ -8,41 +8,71 @@ lastx = x
 objectLocated = 0
 whichObject = 0
 switchCounter = 0
-def locateObjects():
-    global x, y, objectLocated, whichObject, switchCounter
-    page = urllib.request.urlopen('http://10.1.1.8/CmdChannel?TRIG')
-    time.sleep(2)
-    page = urllib.request.urlopen('http://10.1.1.8/CmdChannel?gRES')
+
+
+def handleCameraOutput(side, page):
     #reads output from camera
     coords = page.read().decode('utf-8')
     #splits output
     x1 = coords.split(",")
     whichObject = int(x1[1])
     objectLocated = int(x1[2])
-    if objectLocated == 1:
-        switchCounter = 0
-        y = x1[4]
-        x = x1[3]
-        x = (float(x) + 25) /1000
-        y = (float(y) - 385) /1000
-        time.sleep(3)
-        print(x, y)
-        locateObjects()
-    
+    # check if no block
+    if len(x1) < 2:
+        # No block found
+        continue
+    else:
+        # NO. Blocks > 0
+        if objectLocated == 1:
+            switchCounter = 0
+            y = x1[4]
+            x = x1[3]
+            x = (float(x) + 25) /1000
+            y = (float(y) - 385) /1000
+            time.sleep(3)
+            print(x, y)
+            if side == "left": 
+                locateObjectsLeft()
+            if side == "right": 
+                locateObjectsRight()
+        
+
+def locateObjectsLeft():
+    global x, y, objectLocated, whichObject, switchCounter
+    page = urllib.request.urlopen('http://10.1.1.8/CmdChannel?TRIG')
+    time.sleep(2)
+    page = urllib.request.urlopen('http://10.1.1.8/CmdChannel?gRES')
+    handleCameraOutput("left", page)
+
+   
+
+def locateObjectsRight():
+    global x, y, objectLocated, whichObject, switchCounter
+    page = urllib.request.urlopen('http://10.1.1.7/CmdChannel?TRIG')
+    time.sleep(2)
+    page = urllib.request.urlopen('http://10.1.1.7/CmdChannel?gRES')
+    handleCameraOutput("right", page)
+   
+        
   
 def switchObject():
     global whichObject, switchCounter
     switchCounter += 1
     if whichObject == 0:
+        # Looks for Type 1 object
         page = urllib.request.urlopen('http://10.1.1.8/CmdChannel?sINT_1_1')
         time.sleep(3)
     if whichObject == 1:
+        # Looks for Type 2 object
         page = urllib.request.urlopen('http://10.1.1.8/CmdChannel?sINT_1_0')
         time.sleep(3)
     time.sleep(1)
     print("object switched")
     
     
-while switchCounter < 3:
-    locateObjects()
-    switchObject()
+#while switchCounter < 3:
+#    locateObjects()
+#    switchObject()
+
+
+
